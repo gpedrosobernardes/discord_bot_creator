c76da6917c92516a5d0723217e421162c4202630
gustavopedrosob <gpedrosobernardes@gmail.com>
2026-01-23 12:05:59 -0300

    refactor: GroupView inherits QDialog and implements translate_ui

--- views/group.py
+++ views/group.py
@@ -16,20 +16,25 @@ from qextrawidgets.icons import QThemeResponsiveIcon
 
 from widgets.channel_dialog import ChannelDialog
 
-translate = QCoreApplication.translate
 
-
-class GroupView:
+class GroupView(QDialog):
     def __init__(self):
-        self.window = QDialog()
-        self.window.setWindowFlags(
-            self.window.windowFlags() | Qt.WindowType.WindowMaximizeButtonHint
-        )
-        self.window.setWindowIcon(QIcon("source/icons/window-icon.svg"))
-        self.window.setMinimumSize(800, 600)
-        self.window.resize(1000, 800)
+        super().__init__()
+        self.setWindowFlags(self.windowFlags() | Qt.WindowType.WindowMaximizeButtonHint)
+        self.setWindowIcon(QIcon("source/icons/window-icon.svg"))
+        self.setMinimumSize(800, 600)
+        self.resize(1000, 800)
 
         self.channel_pick_dialog = ChannelDialog()
+
+        self._init_ui()
+        self._init_layout()
+        self.translate_ui()
+
+    def _init_ui(self):
+        self.welcome_message_label = QLabel()
+        self.welcome_message_channel_label = QLabel()
+        self.welcome_message_message_label = QLabel()
 
         self.welcome_message_channels = QComboBox()
         self.welcome_message_textedit = QPlainTextEdit()
@@ -39,6 +44,10 @@ class GroupView:
             QThemeResponsiveIcon.fromAwesome("fa6s.list")
         )
         self.welcome_message_pick_button.setIconSize(QSize(19, 19))
+
+        self.goodbye_message_label = QLabel()
+        self.goodbye_message_channel_label = QLabel()
+        self.goodbye_message_message_label = QLabel()
 
         self.goodbye_message_channels = QComboBox()
         self.goodbye_message_textedit = QPlainTextEdit()
@@ -51,39 +60,38 @@ class GroupView:
         self.goodbye_message_pick_button.setIconSize(QSize(19, 19))
 
         self.save_button = QColorButton("#3DCC61")
-        self.save_button.setText(translate("MessageWindow", "Confirm and save"))
         self.save_button.setIcon(QThemeResponsiveIcon.fromAwesome("fa6s.floppy-disk"))
 
-        self.setup_layout()
-
-    def setup_layout(self):
+    def _init_layout(self):
         main_layout = QFormLayout()
-        main_layout.addRow(
-            QLabel(translate("GroupWindow", "Welcome message:")),
-        )
+        main_layout.addRow(self.welcome_message_label)
+
         welcome_message_channel_layout = QHBoxLayout()
         welcome_message_channel_layout.addWidget(
             self.welcome_message_channels, stretch=1
         )
         welcome_message_channel_layout.addWidget(self.welcome_message_pick_button)
         main_layout.addRow(
-            translate("GroupWindow", "Channel:"), welcome_message_channel_layout
+            self.welcome_message_channel_label, welcome_message_channel_layout
         )
         main_layout.addRow(
-            QLabel(translate("GroupWindow", "Message:")), self.welcome_message_textedit
+            self.welcome_message_message_label, self.welcome_message_textedit
         )
-        main_layout.addRow(QLabel(translate("GroupWindow", "Goodbye message:")))
+
+        main_layout.addRow(self.goodbye_message_label)
+
         goodbye_message_channel_layout = QHBoxLayout()
         goodbye_message_channel_layout.addWidget(
             self.goodbye_message_channels, stretch=1
         )
         goodbye_message_channel_layout.addWidget(self.goodbye_message_pick_button)
         main_layout.addRow(
-            translate("GroupWindow", "Channel:"), goodbye_message_channel_layout
+            self.goodbye_message_channel_label, goodbye_message_channel_layout
         )
         main_layout.addRow(
-            translate("GroupWindow", "Message:"), self.goodbye_message_textedit
+            self.goodbye_message_message_label, self.goodbye_message_textedit
         )
+
         main_layout.addItem(
             QSpacerItem(
                 0, 0, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding
@@ -94,4 +102,16 @@ class GroupView:
             self.save_button, alignment=Qt.AlignmentFlag.AlignRight
         )
         main_layout.addItem(buttons_layout)
-        self.window.setLayout(main_layout)
+        self.setLayout(main_layout)
+
+    def translate_ui(self):
+        self.setWindowTitle(self.tr("Group Configuration"))
+        self.welcome_message_label.setText(self.tr("Welcome message:"))
+        self.welcome_message_channel_label.setText(self.tr("Channel:"))
+        self.welcome_message_message_label.setText(self.tr("Message:"))
+
+        self.goodbye_message_label.setText(self.tr("Goodbye message:"))
+        self.goodbye_message_channel_label.setText(self.tr("Channel:"))
+        self.goodbye_message_message_label.setText(self.tr("Message:"))
+
+        self.save_button.setText(self.tr("Confirm and save"))
